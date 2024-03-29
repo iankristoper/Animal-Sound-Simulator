a9c17a7f78dd85f47648fb4fa626bd2f592ae1b2
iankristoper <iankristoper26@gmail.com>
2024-03-29 17:08:11 +0800

    UPDATE: base class features

---
+++ src/main/java/com/ian/animal_sound/Animal.java
@@ -0,0 +1,13 @@
+package com.ian.animal_sound;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+//base class for the animal sounds 
+public abstract class Animal 
+{
+    //methods for all sounds 
+    public abstract void makeSound();
+}
